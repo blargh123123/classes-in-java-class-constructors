923ba9fee643fb4b8e3d5a98f5fa4557a75fa63b
Doug Hughes <dhughes@doughughes.net>
2017-02-20 10:40:45 -0500

    complete exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,31 @@
 public class Main {
 
     // todo: Create a main() method.
+    public static void main(String[] args) {
 
         // todo: Create a new instance of the Character class named wizard. The instance's type should be set to 'wizard', but all other properties are up to you.
+        Character wizard = new Character("Fred", "wizard", 123, 23);
 
         // todo: Create a new instance of the Character class named orc. The instance's type should be set to 'orc', but all other properties are up to you.
+        Character orc = new Character("Sven", "orc", 231, 43);
 
         // todo: Print the wizard to the console.
+        System.out.println(wizard);
 
         // todo: Print the orc to the console.
+        System.out.println(orc);
 
         // todo: Have the wizard attack the orc
+        wizard.attack(orc);
 
         // todo: Have the orc attack the wizard
+        orc.attack(wizard);
 
         // todo: Print the wizard to the console again.
+        System.out.println(wizard);
 
         // todo: Print the orc to the console again.
+        System.out.println(orc);
 
+    }
 }
